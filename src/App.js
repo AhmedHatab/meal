fb9c123d0e1368a15392073ccd02ece142659a35
AhmedHatab <ahmedehatab@gmail.com>
2023-01-04 02:01:51 +0200

    update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Navbar />
           
             <Routes>
-                <Route path='/' element={<Home />} ></Route>
+                <Route path='' element={<Home />} ></Route>
                 <Route path='home' element={<Home />} ></Route>
                 <Route path='about' element={<About />} ></Route>
                 <Route path='menu' element={<Menu />} ></Route>
